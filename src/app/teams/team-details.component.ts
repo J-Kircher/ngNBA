b6c5d151593d13b1c822a500c2b382c598ac82d2
Jim <James.C.KIrcher@gmail.com>
2020-03-20 09:51:59 -0400

    Update team details to use mat-table

--- src/app/teams/team-details.component.ts
+++ src/app/teams/team-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
 import { TeamService } from '@app/service/team.service';
 import { PlayoffService } from '@app/service/playoff.service';
 import { ITeam } from '../model/nba.model';
@@ -15,6 +16,10 @@ export class TeamDetailsComponent implements OnInit {
   teamIdx: number;
   total: number;
   NBAChamp: number;
+  teamExt;
+  attColumns: string[] = [];
+  statsColumns: string[] = [];
+  dataSource = new MatTableDataSource();
 
   constructor(
     private teamService: TeamService,
@@ -27,7 +32,18 @@ export class TeamDetailsComponent implements OnInit {
       this.teamIdx = this.teamService.getTeamIndex(params['abbrev']);
       this.team = this.teamService.getTeam(params['abbrev']);
       this.total = this.team.of + this.team.de + this.team.co;
+      this.teamExt = JSON.parse(JSON.stringify(this.team));
+      this.teamExt.apf = this.myRound(this.team.pf / (this.team.wins + this.team.losses), 1);
+      this.teamExt.apa = this.myRound(this.team.pa / (this.team.wins + this.team.losses), 1);
     });
     this.playoffService.NBAChamp$.subscribe(data => this.NBAChamp = data);
+    this.attColumns = ['of', 'de', 'co', 'total'];
+    this.statsColumns = ['wins', 'losses', 'apf', 'apa'];
+    this.dataSource = new MatTableDataSource([this.teamExt]);
+  }
+
+  myRound(value: number, precision: number): number {
+    const multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
   }
 }
